95f6f8e7324622484801fa9d4c68cfb19df5ab10
Luc Martens <luc.martens@greenhousegroup.com>
2018-08-25 19:01:43 +0200

    Extend parser support for events (#3)

--- README.md
+++ README.md
@@ -3,11 +3,12 @@
 - [x] Single machine
 - [x] Parallel machine
 - [ ] Hierarchical machine
-- [ ] Initial event
-- [ ] internal events
+- [x] Initial event
+- [x] Internal string events `{on: {x: '.y'}}`
+- [x] Internal obj events `{on {x: {target: 'y'}}}`
 - [x] String event `{on: {x: 'y'}}`
-- [ ] Object event `{on: {x: {y: {}}}}`
-- [ ] Array event `{on: {x: [{target: 'y'}]}}`
+- [x] Object event `{on: {x: {y: {}}}}`
+- [x] Array event `{on: {x: [{target: 'y'}]}}`
 - [ ] String action `fetch`
 - [ ] Object action `{ type: 'fetch'}`
 - [ ] Function action
--- src/parser.js
+++ src/parser.js
@@ -1,47 +1,84 @@
 const _ = require('lodash/fp');
 
-const state = (prefix, states) =>
+const combinations = (collA, collB) =>
+  _.flatMap(a => _.map(b => [a, b], collB), collA);
+
+const state = (prefix, { states }) =>
   _.reduce(
     (acc, [k, v]) => [...acc, ['state', [k, `${prefix}_${k}`]]],
     [],
     _.toPairs(states)
   );
 
-const events = (prefix, { states, initial }) =>
-  _.reduce(
-    (acc, [from, v]) => [
-      ...acc,
-      ..._.map(
-        ([event, target]) => [
-          'event',
-          [`${prefix}_${from}`, `${prefix}_${target}`, event]
-        ],
-        _.toPairs(v.on)
-      )
-    ],
-    [],
-    _.toPairs(states)
+const events = (prefix, config) => {
+  const event = from => ([ev, target]) => {
+    if (_.isString(target)) {
+      return ['event', [`${prefix}_${from}`, `${prefix}_${target}`, ev]];
+    }
+
+    if (_.isArray(target)) {
+      const name = _.get('0.target', target);
+      return ['event', [`${prefix}_${from}`, `${prefix}_${name}`, ev]];
+    }
+
+    if (_.isObject(target) && !target.internal) {
+      const name = _.keys(target)[0];
+      return ['event', [`${prefix}_${from}`, `${prefix}_${name}`, ev]];
+    }
+
+    if (_.isObject(target) && !!target.internal) {
+      const name = target.target;
+      return ['event', [`${prefix}_${from}`, `${prefix}_${name}`, ev]];
+    }
+  };
+
+  let ast = config.initial
+    ? [['event', ['[*]', `${prefix}_${config.initial}`]]]
+    : [];
+
+  ast = _.reduce(
+    (acc, [from, v]) => [...acc, ..._.map(event(from), _.toPairs(v.on))],
+    ast,
+    _.toPairs(config.states)
   );
 
-const mach = xstate => [
+  if (config.on) {
+    ast = _.reduce(
+      (acc, [[from], [ev, target]]) => {
+        const x = target[0] === '.' ? target.substr(1) : target;
+        return [...acc, event(from)([ev, x])];
+      },
+      ast,
+      combinations(_.toPairs(config.states), _.toPairs(config.on))
+    );
+  }
+
+  return ast;
+};
+
+const machine = xstate => [
   'machine',
   { key: xstate.key },
-  ...state(xstate.key, xstate.states),
+  ...state(xstate.key, xstate),
   ...events(xstate.key, xstate)
 ];
 
 const root = xstate => {
   if (!xstate.parallel) {
-    return ['root', {}, mach(xstate)];
+    return ['root', {}, machine(xstate)];
   }
 
   const machines = _.map(
-    ([key, machine]) => ({ ...machine, key }),
+    ([key, config]) => ({ ...config, key }),
     _.toPairs(xstate.states)
   );
 
-  console.log(machines);
-  return ['root', {}, ..._.map(mach, machines)];
+  return ['root', {}, ..._.map(machine, machines)];
 };
 
-module.exports.default = root;
+module.exports = {
+  state,
+  events,
+  machine,
+  root
+};
